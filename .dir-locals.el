2e2d06468bd3481d4032da804cf1b1dcdf38e5f3
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2019-03-22 10:40:59 +0100

    Adaptando el dir-locals.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((projectile-project-compilation-cmd . "cmake ..")
          (projectile-project-compilation-dir . "build/")
+         (projectile-project-run-cmd . "~/programacion/qt/divenu/build/divenu")
          )))
